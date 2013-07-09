31bc70f6cdd62666d1f0078e2d1e158ece3b0a05
yaohuang <yao.huang@live.com>
2013-07-09 14:46:00 -0700

    Email client.

--- LunchBuddies/EmailClient.cs
+++ LunchBuddies/EmailClient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
@@ -11,18 +12,25 @@ namespace LunchBuddies
     public static class EmailClient
     {
         private static HttpClient _client = new HttpClient();
+        private static string _emailClient = ConfigurationManager.AppSettings["EmailClientUser"];
+        private static string _emailClientKey = ConfigurationManager.AppSettings["EmailClientKey"];
 
-        public static Task<HttpResponseMessage> SendEmailAsync()
+        public static Task<HttpResponseMessage> SendEmailAsync(string destination, string content)
         {
             UriBuilder uri = new UriBuilder("https://sendgrid.com/api/mail.send.json");
             StringBuilder query = new StringBuilder();
 
-            query.AppendFormat("api_user={0}", HttpUtility.UrlEncode("azure_8461cc7e7ed7d06de774dc84ca84bfe8@azure.com"));
-            query.AppendFormat("&api_key={0}", HttpUtility.UrlEncode("di3nxta3"));
+            if (String.IsNullOrEmpty(_emailClient) || String.IsNullOrEmpty(_emailClientKey))
+            {
+                throw new InvalidOperationException("EmailClientUser/EmailClientKey is missing from the configuration.");
+            }
+
+            query.AppendFormat("api_user={0}", HttpUtility.UrlEncode(_emailClient));
+            query.AppendFormat("&api_key={0}", HttpUtility.UrlEncode(_emailClientKey));
             query.AppendFormat("&from={0}", HttpUtility.UrlEncode("confirmation@lunchbuddies.com"));
-            query.AppendFormat("&to={0}", HttpUtility.UrlEncode("robert.huang.007@gmail.com"));
+            query.AppendFormat("&to={0}", HttpUtility.UrlEncode(destination));
             query.AppendFormat("&subject={0}", HttpUtility.UrlEncode("registration confirmation"));
-            query.AppendFormat("&text={0}", HttpUtility.UrlEncode("Please confirm your account registration by clicking on the link below: "));
+            query.AppendFormat("&text={0}", HttpUtility.UrlEncode(content));
             uri.Query = query.ToString();
             HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, uri.Uri);
             return _client.SendAsync(request);
